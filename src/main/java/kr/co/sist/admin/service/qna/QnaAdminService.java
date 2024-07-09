be8229bd18aa7bb4d9aede9bd55bd9ecae50eedc
psh <ppluvsh@gmail.com>
2024-07-09 20:29:50 +0900

    feat: qna 답변 완료 목록 페이지네이션 구현

--- src/main/java/kr/co/sist/admin/service/qna/QnaAdminService.java
+++ src/main/java/kr/co/sist/admin/service/qna/QnaAdminService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import kr.co.sist.admin.dao.qna.QnaAdminDAO;
 import kr.co.sist.admin.domain.qna.QnaDomain;
 import kr.co.sist.admin.vo.qna.QnaVO;
+import kr.co.sist.admin.vo.qna.SearchVO;
 
 @Service
 public class QnaAdminService {
@@ -20,19 +21,28 @@ public class QnaAdminService {
         this.qnaAdminDAO = qnaAdminDAO;
     }
 
-    // public List<QnaDomain> searchNewQnas() {
-    // List<QnaDomain> qnas = null;
-    // qnas = qnaAdminDAO.selectNewQnas();
-    // return qnas;
+    // public List<Map<String, Object>> searchNewQnas() {
+    // List<QnaDomain> qnas = qnaAdminDAO.selectNewQnas();
+    // List<Map<String, Object>> result = new ArrayList<>();
+    // for (int i = 0; i < qnas.size(); i++) {
+    // QnaDomain qna = qnas.get(i);
+    // Map<String, Object> map = new HashMap<>();
+    // map.put("view_num", i + 1);
+    // map.put("qna_num", qna.getQna_num());
+    // map.put("title", qna.getTitle());
+    // map.put("input_date", qna.getInput_date());
+    // map.put("flag", qna.getFlag());
+    // result.add(map);
     // }
+    // return result;
+    // } // 답변 대기 문의 리스트
 
-    public List<Map<String, Object>> searchNewQnas() {
-        List<QnaDomain> qnas = qnaAdminDAO.selectNewQnas();
+    public List<Map<String, Object>> searchNewQnas(SearchVO sVO) {
+        List<QnaDomain> qnas = qnaAdminDAO.selectNewQnas(sVO);
         List<Map<String, Object>> result = new ArrayList<>();
-        for (int i = 0; i < qnas.size(); i++) {
-            QnaDomain qna = qnas.get(i);
+        for (QnaDomain qna : qnas) {
             Map<String, Object> map = new HashMap<>();
-            map.put("view_num", i + 1);
+            map.put("view_num", qna.getView_num());
             map.put("qna_num", qna.getQna_num());
             map.put("title", qna.getTitle());
             map.put("input_date", qna.getInput_date());
@@ -42,18 +52,15 @@ public class QnaAdminService {
         return result;
     }
 
+    public int countNewQnas() {
+        return qnaAdminDAO.countNewQnas();
+    }
 
     public QnaDomain searchOneNewQna(int qna_num) {
         QnaDomain oneQna = null;
         oneQna = qnaAdminDAO.selectOneNewQna(qna_num);
         return oneQna;
-    }
-
-    // public List<QnaDomain> searchOldQnas() {
-    // List<QnaDomain> oldQnas = null;
-    // oldQnas = qnaAdminDAO.selectOldQnas();
-    // return oldQnas;
-    // }
+    }// 답변 대기 문의 상세 조회
 
     public List<Map<String, Object>> searchOldQnas() {
         List<QnaDomain> qnas = qnaAdminDAO.selectOldQnas();
@@ -69,27 +76,27 @@ public class QnaAdminService {
             result.add(map);
         }
         return result;
-    }
+    }// 답변 완료 문의 리스트 조회
 
 
     public QnaDomain searchOneOldQna(int qna_num) {
         QnaDomain oneOldQna = null;
         oneOldQna = qnaAdminDAO.selectOneOldQna(qna_num);
         return oneOldQna;
-    }
+    }// 답변 완료 문의 상세 조회
 
     public int addQnaAnswer(QnaVO qVO) {
         int result = qnaAdminDAO.insertQnaAnswer(qVO);
         // qnaAdminDAO.updateQnaFlag(qVO.getQna_num());
         return result;
-    }
+    }// 답변하기
 
     public int updateQnaFlag(int qna_num) {
         System.out.println("Updating QNA flag for qna_num: " + qna_num);
         int result2 = qnaAdminDAO.updateQnaFlag(qna_num);
         System.out.println("Update result: " + result2);
         return result2;
-    }
+    }// 답변 플래그 바꾸기
 
 
 
--- src/main/java/kr/co/sist/admin/vo/qna/SearchVO.java
+++ src/main/java/kr/co/sist/admin/vo/qna/SearchVO.java
@@ -1,15 +1,11 @@
 package kr.co.sist.admin.vo.qna;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
 @ToString
 public class SearchVO {
     private String keyword;
@@ -20,4 +16,67 @@ public class SearchVO {
     private int totalPages; // 전체 페이지의 수 항목 수(항목 수와 페이지당 항목 수를 기반으로 계산)
     private int startItemIndex; // 현재 페이지에서 첫 번째로 표시될 항목의 인덱스를 나타내는 변수
     private int endItemIndex; // 현재 페이지에서 마지막으로 표시될 항목의 인덱스를 나타내는 변수
+
+    public void pageIndexes() {
+        this.startItemIndex = (currentPage - 1) * itemsPerPage + 1;
+        this.endItemIndex = currentPage * itemsPerPage;
+
+
+    }
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+
+    public int getCurrentPage() {
+        return currentPage;
+    }
+
+    public void setCurrentPage(int currentPage) {
+        this.currentPage = currentPage;
+    }
+
+    public int getItemsPerPage() {
+        return itemsPerPage;
+    }
+
+    public void setItemsPerPage(int itemsPerPage) {
+        this.itemsPerPage = itemsPerPage;
+    }
+
+    public int getTotalItems() {
+        return totalItems;
+    }
+
+    public void setTotalItems(int totalItems) {
+        this.totalItems = totalItems;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public int getStartItemIndex() {
+        return startItemIndex;
+    }
+
+    public void setStartItemIndex(int startItemIndex) {
+        this.startItemIndex = startItemIndex;
+    }
+
+    public int getEndItemIndex() {
+        return endItemIndex;
+    }
+
+    public void setEndItemIndex(int endItemIndex) {
+        this.endItemIndex = endItemIndex;
+    }
 }
