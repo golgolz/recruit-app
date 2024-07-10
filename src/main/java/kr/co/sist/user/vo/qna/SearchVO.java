ae83e229f4123b936f182d1823bb8219b92ca779
psh <ppluvsh@gmail.com>
2024-07-10 22:59:25 +0900

    fix : 충돌 해결

---
+++ src/main/java/kr/co/sist/user/vo/qna/SearchVO.java
@@ -0,0 +1,80 @@
+package kr.co.sist.user.vo.qna;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class SearchVO {
+    private String keyword;
+    private int currentPage = 1;
+    private int itemsPerPage = 10;
+    private int totalItems;
+    private int totalPages;
+    private int startItemIndex;
+    private int endItemIndex;
+
+    public void pageIndexes() {
+        this.startItemIndex = (currentPage - 1) * itemsPerPage + 1;
+        this.endItemIndex = currentPage * itemsPerPage;
+    }
+
+    // Getters and Setters
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+
+    public int getCurrentPage() {
+        return currentPage;
+    }
+
+    public void setCurrentPage(int currentPage) {
+        this.currentPage = currentPage;
+    }
+
+    public int getItemsPerPage() {
+        return itemsPerPage;
+    }
+
+    public void setItemsPerPage(int itemsPerPage) {
+        this.itemsPerPage = itemsPerPage;
+    }
+
+    public int getTotalItems() {
+        return totalItems;
+    }
+
+    public void setTotalItems(int totalItems) {
+        this.totalItems = totalItems;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public int getStartItemIndex() {
+        return startItemIndex;
+    }
+
+    public void setStartItemIndex(int startItemIndex) {
+        this.startItemIndex = startItemIndex;
+    }
+
+    public int getEndItemIndex() {
+        return endItemIndex;
+    }
+
+    public void setEndItemIndex(int endItemIndex) {
+        this.endItemIndex = endItemIndex;
+    }
+}
